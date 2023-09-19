1b604f5c5e2377d43b611ba5e4ce6de0bf41cea0
Ashish Saini <47918474+ashus3868@users.noreply.github.com>
2023-09-19 07:55:39 +0530

    Update main.c

--- main/main.c
+++ main/main.c
@@ -18,6 +18,7 @@ char *TAG = "BLE-Server";
 uint8_t ble_addr_type;
 
 struct ble_gap_adv_params adv_params;
+bool status = false;
 
 void ble_app_advertise(void);
 
@@ -31,27 +32,27 @@ static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_g
     if (strcmp(data, (char *)"LIGHT ON\0") == 0)
     {
         printf("LIGHT ON\n");
-        gpio_set_level(GPIO_NUM_2,1);
+        gpio_set_level(GPIO_NUM_2, 1);
     }
     else if (strcmp(data, (char *)"LIGHT OFF\0") == 0)
     {
         printf("LIGHT OFF\n");
-        gpio_set_level(GPIO_NUM_2,0);
+        gpio_set_level(GPIO_NUM_2, 0);
     }
     else if (strcmp(data, (char *)"LED ON\0") == 0)
     {
         printf("LED ON\n");
-        gpio_set_level(GPIO_NUM_13,1);
+        gpio_set_level(GPIO_NUM_15, 1);
     }
     else if (strcmp(data, (char *)"LED OFF\0") == 0)
     {
         printf("LED OFF\n");
-        gpio_set_level(GPIO_NUM_13,0);
+        gpio_set_level(GPIO_NUM_15, 0);
     }
-    else
-    {
-        printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
-    }
+    // else
+    // {
+    //     printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
+    // }
     memset(data, 0, strlen(data));
 
     return 0;
@@ -61,6 +62,12 @@ static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_g
 static int device_read(uint16_t con_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
     os_mbuf_append(ctxt->om, "Data from the server", strlen("Data from the server"));
+    return 0;
+}
+
+static int device_read2(uint16_t con_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    os_mbuf_append(ctxt->om, "Message from Innovate Yourself", strlen("Message from Innovate Yourself"));
     return 0;
 }
 
@@ -76,6 +83,9 @@ static const struct ble_gatt_svc_def gatt_svcs[] = {
          {.uuid = BLE_UUID16_DECLARE(0xDEAD), // Define UUID for writing
           .flags = BLE_GATT_CHR_F_WRITE,
           .access_cb = device_write},
+         {.uuid = BLE_UUID16_DECLARE(0xDEAD), // Define UUID for writing
+          .flags = BLE_GATT_CHR_F_READ,
+          .access_cb = device_read2},
          {0}}},
     {0}};
 
@@ -94,7 +104,7 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
         break;
     // Advertise again after completion of the event
     case BLE_GAP_EVENT_DISCONNECT:
-        f("GAP", "BLE GAP EVENT DISCONNECTED");
+        ESP_LOGI("GAP", "BLE GAP EVENT DISCONNECTED");
         if (event->connect.status != 0)
         {
             ble_app_advertise();
@@ -124,7 +134,7 @@ void ble_app_advertise(void)
     ble_gap_adv_set_fields(&fields);
 
     // GAP - device connectivity definition
-    struct ble_gap_adv_params adv_params;
+    // struct ble_gap_adv_params adv_params;
     memset(&adv_params, 0, sizeof(adv_params));
     // adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
     // adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
@@ -167,17 +177,17 @@ void boot_creds_clear(void *param)
     {
 
         if (!gpio_get_level(GPIO_NUM_0))
-
         {
             int64_t n = esp_timer_get_time();
 
             if ((n - m) / 1000 >= 2000)
             {
                 ESP_LOGI("BOOT BUTTON:", "Button Pressed FOR 3 SECOND\n");
+
                 adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
                 adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
                 ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
-
+                status = true;
                 vTaskDelay(100);
                 m = esp_timer_get_time();
             }
@@ -187,15 +197,22 @@ void boot_creds_clear(void *param)
             m = esp_timer_get_time();
         }
         vTaskDelay(10);
-        
+
+        if (status)
+        {
+            // ESP_LOGI("GAP", "BLE GAP status");
+            adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
+            adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
+            ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
+        }
     }
 }
 
 void app_main()
 {
-    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT); // boot button
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
-    gpio_set_direction(GPIO_NUM_13, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT);
     connect_ble();
     xTaskCreate(boot_creds_clear, "boot_creds_clear", 2048, NULL, 5, NULL);
 }
